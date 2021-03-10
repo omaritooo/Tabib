fdf3297b90791dd3cf81150c00dcf3257c9b861a
omaritooo <omaritooo@users.noreply.github.com>
2021-03-11 00:44:05 +0200

    system

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,7 @@
-function isElementInViewport(elem) {
-    var $elem = $(elem);
-
-    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
-    var viewportTop = $(scrollElem).scrollTop();
-    var viewportBottom = viewportTop + $(window).height();
-
-    var elemTop = Math.round( $elem.offset().top );
-    var elemBottom = elemTop + $elem.height();
-
-    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
-}
-
-function checkAnimation() {
-    var $elem = $('.segment1 .cat2');
-
-    if ($elem.hasClass('animate__animated animate__bounce')) return;
-
-    if (isElementInViewport($elem)) {
-        $elem.addClass('animate__animated animate__bounce');
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 20) {
+        $(".topnav").addClass(".navbar-default");
+    } else {
+       $(".topnav").removeClass(".navbar-default");
     }
-}
-
-$(window).scroll(function(){
-    checkAnimation();
 });
