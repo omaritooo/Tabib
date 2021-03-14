b6e1395175018d1799cbf606a768e51496834468
omaritooo <omaritooo@users.noreply.github.com>
2021-03-14 12:04:06 +0200

    v2

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,21 @@
-$(document).on("scroll", function() {
-    if($(document).scrollTop() > 100) {
-        $(".topnav").addClass(".navbar-default");
-    } else {
-       $(".topnav").removeClass(".navbar-default");
-    }
-});
+
+
+
+
+
+
+
+   $(window).on("load",function() {
+      $(window).scroll(function() {
+        var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+        $(".slide-in").each(function() {
+          var objectBottom = $(this).offset().top + $(this).outerHeight();
+          
+          if (objectBottom < windowBottom) { 
+            if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+          } else { 
+            if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+          }
+        });
+      }).scroll(); 
+    });
